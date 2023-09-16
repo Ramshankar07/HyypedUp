75e4a39b86a5d3e730bb51535e189abe4c167045
Ramshankar <87872158+Ramshankar07@users.noreply.github.com>
2023-09-16 17:29:53 +0530

    llm-change

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import os
 # from serp import serpapi
 
 import streamlit as st 
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI,HuggingFaceHub
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 import PyPDF2
 from typing_extensions import Concatenate
 
-os.environ['OPENAI_API_KEY'] = "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"
+# os.environ['OPENAI_API_KEY'] = "sk-EWFb7peDQc"
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = "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"
+
 # os.environ['SERPAPI_API_KEY'] = serpapi
 
 
@@ -24,7 +26,7 @@ if uploaded_file is not None:
     # read text from pdf
     raw_text = ''
     for i, page in enumerate(df.pages):
-        content = page.extract_text()
+
         if content:
             raw_text += content
 #text splitter
@@ -34,7 +36,7 @@ if uploaded_file is not None:
 # Prompt templates
 job_template = PromptTemplate(
     input_variables = ['raw_text'], 
-    template='write me a youtube video title about {raw_text} '
+    template='suggest some job title based on this raw text: {raw_text} '
 )
 
 script_template = PromptTemplate(
@@ -48,7 +50,7 @@ script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_his
 
 
 # Llms
-llm = OpenAI(temperature=0.1) 
+llm = HuggingFaceHub(repo_id='google/flan-t5-xl',model_kwargs={"temperature":1e-10})
 title_chain = LLMChain(llm=llm, prompt=job_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
@@ -56,15 +58,16 @@ script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_ke
 
 # # Show stuff to the screen if there's a prompt
 # if prompt: 
-title = title_chain.run(raw_text)
+if uploaded_file is not None:
+    title = title_chain.run(raw_text)
 #     wiki_research = wiki.run(prompt) 
 #     script = script_chain.run(title=title, wikipedia_research=wiki_research)
 
-st.write(title) 
+    st.write(title) 
 #     st.write(script) 
 
-#     with st.expander('Title History'): 
-#         st.info(title_memory.buffer)
+    with st.expander('Title History'): 
+        st.info(title_memory.buffer)
 
 #     with st.expander('Script History'): 
 #         st.info(script_memory.buffer)
