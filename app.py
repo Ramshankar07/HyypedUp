af29499db62f4a088b4b93f8eee2ba6a00314110
Ramshankar <87872158+Ramshankar07@users.noreply.github.com>
2023-09-16 16:51:31 +0530

    Front-end-1

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 import os 
-from apikey import apikey 
-from serp import serpapi
+# from apikey import apikey 
+# from serp import serpapi
 
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
+import PyPDF2
+from typing_extensions import Concatenate
 
-os.environ['OPENAI_API_KEY'] = apikey
-os.environ['SERPAPI_API_KEY'] = serpapi
+os.environ['OPENAI_API_KEY'] = "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"
+# os.environ['SERPAPI_API_KEY'] = serpapi
 
 
 # App framework
 st.title('JobSearch')
-prompt = st.text_input('Plug in your prompt here') 
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+if uploaded_file is not None:
+    df = PyPDF2.PdfReader(uploaded_file)
+#Extracting Resume Data function
+
+    # read text from pdf
+    raw_text = ''
+    for i, page in enumerate(df.pages):
+        content = page.extract_text()
+        if content:
+            raw_text += content
+#text splitter
+
+
 
 # Prompt templates
-title_template = PromptTemplate(
-    input_variables = ['topic'], 
-    template='write me a youtube video title about {topic}'
+job_template = PromptTemplate(
+    input_variables = ['raw_text'], 
+    template='write me a youtube video title about {raw_text} '
 )
 
 script_template = PromptTemplate(
@@ -33,19 +48,19 @@ script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_his
 
 
 # Llms
-llm = OpenAI(temperature=0.9) 
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+llm = OpenAI(temperature=0.1) 
+title_chain = LLMChain(llm=llm, prompt=job_template, verbose=True, output_key='title', memory=title_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
-# wiki = WikipediaAPIWrapper()
+#wiki = WikipediaAPIWrapper()
 
 # # Show stuff to the screen if there's a prompt
 # if prompt: 
-#     title = title_chain.run(prompt)
+title = title_chain.run(raw_text)
 #     wiki_research = wiki.run(prompt) 
 #     script = script_chain.run(title=title, wikipedia_research=wiki_research)
 
-#     st.write(title) 
+st.write(title) 
 #     st.write(script) 
 
 #     with st.expander('Title History'): 
